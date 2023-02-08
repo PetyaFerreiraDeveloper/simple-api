9e90770b518b6b04320c6aa09894bb05a5e2828d
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-02-08 09:08:12 +0100

    test tailwind

--- components/Character.js
+++ components/Character.js
@@ -2,7 +2,7 @@ export default function Character({ character, ind }) {
   const { name, gender } = character;
   return (
     <li key={ind} className="flex gap-2">
-      <a href="">{name}</a>
+      <a className="text-blue-500" href="">{name}</a>
       <p>{gender}</p>
     </li>
   );
